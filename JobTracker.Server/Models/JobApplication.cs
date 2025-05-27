2a65413ae2920b3497deb17e0edf952a53ed4ccc
BrandonBuckner <bbuckner49@hotmail.com>
2025-05-26 21:09:11 -0500

    Began implementing JobApplication model

--- JobTracker.Server/Models/JobApplication.cs
+++ JobTracker.Server/Models/JobApplication.cs
@@ -3,5 +3,23 @@
     public class JobApplication
     {
         //TODO: Define properties
+        private int id;
+        private string companyName;
+        private string role;
+        private string roleDescription;
+        private DateTime applicationDate;
+        private string status;
+        private DateTime lastHeardDate;
+        private DateTime[] interviewDates;
+        private string jobType;
+        private string location;
+        private string salaryEstimate; //Do I do a salary range and an hourly estimate? 
+        private string jobLink;
+        private bool refferal; 
+        private string notes;
+
+        public int Id { get => id; init => id = value; }
+        public string CompanyName { get => companyName; init => companyName = value; }
+        public string Role { get => role; init => role = value; }
     }
-}
+}                                                                                       
