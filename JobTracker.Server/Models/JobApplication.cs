28805517a2cfed7499e4d822c5b3a8af760b1952
BrandonBuckner <bbuckner49@hotmail.com>
2025-06-22 10:37:06 -0500

    Updated model with correct syntax, created constructor, and made a few fields required

--- JobTracker.Server/Models/JobApplication.cs
+++ JobTracker.Server/Models/JobApplication.cs
@@ -2,24 +2,28 @@
 {
     public class JobApplication
     {
-        //TODO: Define properties
-        private int id;
-        private string companyName;
-        private string role;
-        private string roleDescription;
-        private DateTime applicationDate;
-        private string status;
-        private DateTime lastHeardDate;
-        private DateTime[] interviewDates;
-        private string jobType;
-        private string location;
-        private string salaryEstimate; //Do I do a salary range and an hourly estimate? 
-        private string jobLink;
-        private bool refferal; 
-        private string notes;
+        public int Id { get; init; }
+        public string CompanyName { get; init; } = string.Empty;
+        public string Role { get; set; } = string.Empty;
+        public string? RoleDescription { get; set; }
+        public DateTime ApplicationDate { get; set; }
+        public string Status { get; set; } = string.Empty;
+        public DateTime? LastHeardDate { get; set; }
+        public DateTime[]? InterviewDates { get; set; }
+        public string? JobType { get; set; }
+        public string? Location { get; set; }
+        public string? SalaryEstimate { get; set; }
+        public string? JobLink { get; set; }
+        public bool? Referral { get; set; }
+        public string? Notes { get; set; }
 
-        public int Id { get => id; init => id = value; }
-        public string CompanyName { get => companyName; init => companyName = value; }
-        public string Role { get => role; init => role = value; }
+        public JobApplication(int id, string companyName, string role, string status, DateTime applicationDate)
+        {
+            Id = id;
+            CompanyName = companyName ?? throw new ArgumentNullException(nameof(companyName));
+            Role = role ?? throw new ArgumentNullException(nameof(role));
+            Status = status ?? throw new ArgumentNullException(nameof(status));
+            ApplicationDate = applicationDate != default ? applicationDate : throw new ArgumentNullException("Application date cannot be default value.", nameof(ApplicationDate));
+        }
     }
 }                                                                                       
